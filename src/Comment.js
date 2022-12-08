b19c1058d9b5fde9eddbecafe133e0ececd39094
sara sallat <sallatsara@gmail.com>
2022-12-08 16:56:55 +0300

    assignment completed

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,8 @@
 import React from "react";
-
-function Comment() {
-  return <div>Naturally, I agree with this article.</div>;
+class Comment extends React.Component {
+  render() {
+    return <div>Naturally, I agree with this article.</div>;
+  }
 }
 
 export default Comment;
